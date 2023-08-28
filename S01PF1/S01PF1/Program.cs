d646994bb3d8fe815a191c016aff8effd7b97ec4
Pablo Bari <pab.a.bari@gmail.com>
2023-08-28 18:11:34 -0300

    Practicas semanas 1 y 3

---
+++ S01PF1/S01PF1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HolaMundoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int EDAD_MINIMA = 16;
+            int edad;
+            Console.WriteLine("Ingese la edad");
+            edad = int.Parse(Console.ReadLine());
+            if (edad < EDAD_MINIMA)
+            {
+                Console.WriteLine("No podes votar");
+            }
+            else
+            {
+                Console.WriteLine("Podes votar");
+            }
+
+        }
+    }
+}
---
+++ S03PF1/S03PF1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace S03PF1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
